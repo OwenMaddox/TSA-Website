bedd2169e2a83c1edab4b9583f1ec6a2bb5b2a7b
OwenMaddox <owen@maddoxbox.com>
2024-01-29 12:27:41 -0600

    Added more to homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,24 @@
-function Card() {
-  <div></div>;
+function Card(props) {
+  return (
+    <div className="shadow cardbg">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class={props.class}
+              viewBox="0 0 16 16"
+            >
+              <path d={props.d} />
+            </svg>
+          </div>
+          <div class="col">{props.description}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Card;
