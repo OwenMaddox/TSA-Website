61e87a35d2af916acbfb2e6352294ebba4426cd3
OwenMaddox <Owen@maddoxbox.com>
2024-01-29 21:42:13 -0600

    Update several icons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
-import Navbar from "./components/Navbar";
+import Navbar from "./modules/Navbar";
 import Sources from "./pages/Sources";
 
 function App() {
--- src/pages/Sources.js
+++ src/pages/Sources.js
@@ -1,4 +1,4 @@
-import Card from "../components/Card";
+import Card from "../modules/Card";
 
 function Sources() {
   return (
