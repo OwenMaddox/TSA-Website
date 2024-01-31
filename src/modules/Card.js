ee2cd9da20f1f6a5b3a88a4ad3bd25ecd9ac7e33
OwenMaddox <owen@maddoxbox.com>
2024-01-31 11:46:42 -0600

    Flippy Cards

--- src/modules/Card.js
+++ src/modules/Card.js
@@ -1,24 +1,33 @@
+import "./CardFlipper.css";
 function Card(props) {
   return (
-    <div className="shadow p-3 mb-5 cardbg ">
-      <div class="container text-center ">
-        <div class="row ">
-          <div class="col ">
-            <h1 className="title  fs-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="50"
-                height="50"
-                fill="currentColor"
-                class={props.class}
-                viewBox="0 0 16 16"
-              >
-                <path d={props.d} />
-              </svg>
-              {props.title}
-            </h1>
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <div className="shadow p-3 mb-5 cardbg ">
+            <div class="container text-center row">
+              <div class="col ">
+                <h1 className="title  fs-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="40"
+                    height="40"
+                    fill="currentColor"
+                    class={props.class}
+                    viewBox="0 0 16 16"
+                  >
+                    <path d={props.d} />
+                  </svg>
+                </h1>
+              </div>
+              <div className="col subTitle fs-5">{props.title}</div>
+            </div>
+            <h1 className="fs-6">{props.description}</h1>
           </div>
-          <div className="col subTitle fs-4">{props.description}</div>
+        </div>
+        <div className="flip-card-back cardbg shadow">
+          <h1 className="title fs-3">{props.backTitle}</h1>
+          <p>{props.backDescription}</p>
         </div>
       </div>
     </div>
